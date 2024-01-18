f336153f99e7d574225b514bb1a98d87dad06b75
bachtiaralamsyah10@gmail.com <bachtiaralamsyah10@gmail.com>
2024-01-18 10:14:26 +0700

    Menambahkan tampilan CreateProduct

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Login from "./Pages/Login";
 import Register from "./Pages/Register";
 import DaftarProduct from "./Pages/DaftarProduct";
+import CreateProduct from "./Pages/CreateProduct";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
           <Route path="/" element={<DaftarProduct />}></Route>
           <Route path="/register" element={<Register />}></Route>
           <Route path="/login" element={<Login />}></Route>
+          <Route path="/CreateProduct" element={<CreateProduct />}></Route>
         </Routes>
       </Router>
     </>
